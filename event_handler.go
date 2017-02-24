a3f11393b25350ba9f7fade423dde6786aa4565d
Kentaro Kawano <kentaro.kawano@gmail.com>
2017-02-24 19:56:39 +0900

    implements handler

--- event_handler.go
+++ event_handler.go
@@ -1,81 +1,151 @@
 package wrapper
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 // Handler is a Handler
 type Handler struct {
-	handlerFuncMap map[linebot.EventType]func(*linebot.Event)
+	client *linebot.Client
 
 	// Message Event
-	textMessageHandlerFunc     func(*linebot.Event, *linebot.TextMessage)
-	imageMessageHandlerFunc    func(*linebot.Event, *linebot.ImageMessage)
-	videoMessageHandlerFunc    func(*linebot.Event, *linebot.VideoMessage)
-	audioMessageHandlerFunc    func(*linebot.Event, *linebot.AudioMessage)
-	locationMessageHandlerFunc func(*linebot.Event, *linebot.LocationMessage)
-	stickerMessageHandlerFunc  func(*linebot.Event, *linebot.StickerMessage)
+	OnTextMessageRecieved     TextMessageHandlerFunc
+	OnImageMessageRecieved    ImageMessageHandlerFunc
+	OnVideoMessageRecieved    VideoMessageHandlerFunc
+	OnAudioMessageRecieved    AudioMessageHandlerFunc
+	OnLocationMessageRecieved LocationMessageHandlerFunc
+	OnStickerMessageRecieved  StickerMessageHandlerFunc
+
+	// Follow/Unfollow Event
+	OnFollowed   FollowEventHandlerFunc
+	OnUnfollowed UnfollowEventHandlerFunc
+
+	// Join/Leave Event
+	OnJoinGroup     JoinGroupEventHandlerFunc
+	OnLeaveGroup    LeaveGroupEventHandlerFunc
+	OnJoinTalkRoom  JoinTalkRoomEventHandlerFunc
+	OnLeaveTalkRoom LeaveTalkRoomEventHandlerFunc
+
+	// Postback Event
+	OnPostback PostbackEventHandlerFunc
 
 	// Beacon Event
-	beaconEnterHandlerFunc func(*linebot.Event)
-	beaconLeaveHandlerFunc func(*linebot.Event)
+	OnBeaconEnter        BeaconEnterEventHandlerFunc
+	OnBeaconLeave        BeaconLeaveEventHandlerFunc
+	OnBeaconBannerTapped BeaconBannerEventHandlerFunc
+
+	OnError func(http.ResponseWriter, *http.Request, error)
+}
+
+// New returns a Handler using *linebot.Client
+func New(client *linebot.Client) (*Handler, error) {
+	if client == nil {
+		return nil, fmt.Errorf("linebot.Client is nil")
+	}
+
+	return &Handler{
+		client: client,
+	}, nil
+}
+
+func (handler *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	events, err := handler.client.ParseRequest(r)
+	if err == linebot.ErrInvalidSignature {
+		w.WriteHeader(400)
+		return
+	} else if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	handler.HandleEvents(events)
 }
 
 // HandleEvents handles events
-func (handler *Handler) HandleEvents(events []*linebot.Event, r *http.Request) {
-	for _, event := range events {
+func (handler *Handler) HandleEvents(events []*linebot.Event) {
+	var wg sync.WaitGroup
 
-		f, ok := handler.handlerFuncMap[event.Type]
-		if ok {
-			f(event)
-			continue
+	for _, event := range events {
+		wg.Add(1)
+		go func(event *linebot.Event) {
+			if err := handler.handleEvent(event); err != nil {
+				log.Print(err)
+			}
+			wg.Done()
+		}(event)
+	}
+	wg.Wait()
+}
+
+func (handler *Handler) handleEvent(event *linebot.Event) error {
+
+	var replyMessages []linebot.Message
+
+	switch event.Type {
+	case linebot.EventTypeMessage:
+
+		switch message := event.Message.(type) {
+		case *linebot.TextMessage:
+			replyMessages = handler.OnTextMessageRecieved(event, message)
+		case *linebot.ImageMessage:
+			replyMessages = handler.OnImageMessageRecieved(event, message)
+		case *linebot.VideoMessage:
+			replyMessages = handler.OnVideoMessageRecieved(event, message)
+		case *linebot.AudioMessage:
+			replyMessages = handler.OnAudioMessageRecieved(event, message)
+		case *linebot.LocationMessage:
+			replyMessages = handler.OnLocationMessageRecieved(event, message)
+		case *linebot.StickerMessage:
+			replyMessages = handler.OnStickerMessageRecieved(event, message)
 		}
 
-		switch event.Type {
-		case linebot.EventTypeMessage:
+	case linebot.EventTypeFollow:
+		replyMessages = handler.OnFollowed(event, event.Source.UserID)
 
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if handler.textMessageHandlerFunc != nil {
-					handler.textMessageHandlerFunc(event, message)
-				}
-			case *linebot.ImageMessage:
-				if handler.imageMessageHandlerFunc != nil {
-					handler.imageMessageHandlerFunc(event, message)
-				}
-			case *linebot.VideoMessage:
-				if handler.videoMessageHandlerFunc != nil {
-					handler.videoMessageHandlerFunc(event, message)
-				}
-			case *linebot.AudioMessage:
-				if handler.audioMessageHandlerFunc != nil {
-					handler.audioMessageHandlerFunc(event, message)
-				}
-			case *linebot.LocationMessage:
-				if handler.locationMessageHandlerFunc != nil {
-					handler.locationMessageHandlerFunc(event, message)
-				}
-			case *linebot.StickerMessage:
-				if handler.stickerMessageHandlerFunc != nil {
-					handler.stickerMessageHandlerFunc(event, message)
-				}
-			}
-		case linebot.EventTypeBeacon:
-			if event.Beacon == nil {
-				break
-			}
-			switch event.Beacon.Type {
-			case linebot.BeaconEventTypeEnter:
-				if handler.beaconEnterHandlerFunc != nil {
-					handler.beaconEnterHandlerFunc(event)
-				}
-			case linebot.BeaconEventTypeLeave:
-				if handler.beaconLeaveHandlerFunc != nil {
-					handler.beaconLeaveHandlerFunc(event)
-				}
-			}
+	case linebot.EventTypeUnfollow:
+		replyMessages = handler.OnUnfollowed(event, event.Source.UserID)
+
+	case linebot.EventTypeJoin:
+		switch event.Source.Type {
+		case linebot.EventSourceTypeGroup:
+			replyMessages = handler.OnJoinGroup(event, event.Source.GroupID)
+		case linebot.EventSourceTypeRoom:
+			replyMessages = handler.OnJoinTalkRoom(event, event.Source.RoomID)
+		}
+
+	case linebot.EventTypeLeave:
+		switch event.Source.Type {
+		case linebot.EventSourceTypeGroup:
+			replyMessages = handler.OnLeaveGroup(event, event.Source.GroupID)
+		case linebot.EventSourceTypeRoom:
+			replyMessages = handler.OnLeaveTalkRoom(event, event.Source.RoomID)
+		}
+
+	case linebot.EventTypePostback:
+		replyMessages = handler.OnPostback(event, event.Postback.Data)
+
+	case linebot.EventTypeBeacon:
+		switch event.Beacon.Type {
+		case linebot.BeaconEventTypeEnter:
+			replyMessages = handler.OnBeaconEnter(event, event.Beacon.Hwid)
+		case linebot.BeaconEventTypeLeave:
+			replyMessages = handler.OnBeaconLeave(event, event.Beacon.Hwid)
+		case linebot.BeaconEventTypeBanner:
+			replyMessages = handler.OnBeaconBannerTapped(event, event.Beacon.Hwid)
 		}
 	}
+
+	if len(replyMessages) > 0 {
+		_, err := handler.client.ReplyMessage(event.ReplyToken, replyMessages...).Do()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ types.go
@@ -0,0 +1,67 @@
+package wrapper
+
+import "github.com/line/line-bot-sdk-go/linebot"
+
+// TextMessageHandlerFunc is a function to handle a linebot.TextMessage.
+// If It requires reply messages it returns them.
+type TextMessageHandlerFunc func(*linebot.Event, *linebot.TextMessage) []linebot.Message
+
+// ImageMessageHandlerFunc is a function to handle a linebot.ImageMessage.
+// If It requires reply messages it returns them.
+type ImageMessageHandlerFunc func(*linebot.Event, *linebot.ImageMessage) []linebot.Message
+
+// VideoMessageHandlerFunc is a function to handle a linebot.VideoMessage.
+// If It requires reply messages it returns them.
+type VideoMessageHandlerFunc func(*linebot.Event, *linebot.VideoMessage) []linebot.Message
+
+// AudioMessageHandlerFunc is a function to handle a linebot.AudioMessage.
+// If It requires reply messages it returns them.
+type AudioMessageHandlerFunc func(*linebot.Event, *linebot.AudioMessage) []linebot.Message
+
+// LocationMessageHandlerFunc is a function to handle a linebot.LocationMessage.
+// If It requires reply messages it returns them.
+type LocationMessageHandlerFunc func(*linebot.Event, *linebot.LocationMessage) []linebot.Message
+
+// StickerMessageHandlerFunc is a function to handle a linebot.StickerMessage.
+// If It requires reply messages it returns them.
+type StickerMessageHandlerFunc func(*linebot.Event, *linebot.StickerMessage) []linebot.Message
+
+// FollowEventHandlerFunc is a function to handle follow event.
+// Second argument is userId.
+type FollowEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// UnfollowEventHandlerFunc is a function to handle unfollow event.
+// Second argument is userId.
+type UnfollowEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// JoinGroupEventHandlerFunc is a function to handle event when join the group.
+// Second argument is the groupId.
+type JoinGroupEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// LeaveGroupEventHandlerFunc is a function to handle event when leave the group.
+// Second argument is the groupId.
+type LeaveGroupEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// JoinTalkRoomEventHandlerFunc is a function to handle event when join the talk room.
+// Second argument is the roomId.
+type JoinTalkRoomEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// LeaveTalkRoomEventHandlerFunc is a function to handle event when leave the talk room.
+// Second argument is the roomId.
+type LeaveTalkRoomEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// PostbackEventHandlerFunc is a function to handle postback event.
+// Second argument is the postback.data.
+type PostbackEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// BeaconEnterEventHandlerFunc is a function to handle event when enter the range of a beacon.
+// Second argument is the beacon.hwid.
+type BeaconEnterEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// BeaconLeaveEventHandlerFunc is a function to handle event when leave the range of a beacon.
+// Second argument is the beacon.hwid.
+type BeaconLeaveEventHandlerFunc func(*linebot.Event, string) []linebot.Message
+
+// BeaconBannerEventHandlerFunc is a function to handle event when user tapped beacon banner.
+// Second argument is the beacon.hwid.
+type BeaconBannerEventHandlerFunc func(*linebot.Event, string) []linebot.Message
